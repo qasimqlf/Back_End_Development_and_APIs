8df43991c3b13aba0fa27d781df6ec14c601fe6f
Qasim Qlf <qlfqasim@gmail.com>
2022-09-26 00:39:33 +0500

    Update myApp.js

--- Course03 - MongoDB and Mongoose/myApp.js
+++ Course03 - MongoDB and Mongoose/myApp.js
@@ -1,10 +1,32 @@
 require('dotenv').config();
+let mongoose = require('mongoose');
+const mySecret = process.env['MONGO_URI'];
+mongoose.connect(mySecret, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const personSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: Number,
+    favoriteFoods: [String]
+})
 
-let Person;
+let Person = mongoose.model('Person', personSchema);
+let qasim = new Person({ name: 'Mr Qasim', age: 25, favoriteFoods: ['Biryani'] });
+let andy = new Person({ name: 'Andy', age: 24, favoriteFoods: ['Meat'] });
+let arrayOfPeople = [qasim, andy];
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  //   ...do your stuff here...
+  qasim.save(function(err, data) {
+    if(err) {
+      done(err);
+    } else {
+      done(null, data);
+    }
+  });
+  
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
